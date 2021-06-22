9b9a1b3119710566810f38a198162ac866148b62
matheus-fernandes <matheusfsilva10@gmail.com>
2021-06-22 16:14:59 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState, createContext, useEffect } from 'react'
+import { Home } from "./pages/Home";
+import { NewRoom } from './pages/NewRoom';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import firebase from "firebase";
+import { auth } from "./services/firebase";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new/" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
